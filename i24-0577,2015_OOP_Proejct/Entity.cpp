885753aba0e39918b6be00b54c836154795eb444
Muhammad Qasim Chohan <i240577@isb.nu.edu.pk>
2026-02-27 12:37:10 +0500

    Delete i24-0577,2015_OOP_Proejct directory

--- i24-0577,2015_OOP_Proejct/Entity.cpp
+++
@@ -1,9 +0,0 @@
-#include "Entity.h"
-
-Entity::Entity()
-    : position(0.0f, 0.0f),
-    health(100),
-    maxHealth(100) {
-}
-
-Entity::~Entity() = default;
--- i24-0577,2015_OOP_Proejct/Utils.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-template <typename T>
-T clamp(T value, T min, T max) {
-    if (value < min) return min;
-    if (value > max) return max;
-    return value;
-}
